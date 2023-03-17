d83e59095f4fa7b346868bcf2f7916cd263b22af
Wojceich Jankowski <wjankowski@student.agh.edu.pl>
2023-03-17 21:38:52 +0100

    Zamiana w kodzie głownym dodadanie obslugi maila

--- main.py
+++ main.py
@@ -12,14 +12,37 @@ Tryb ciemny
 Wysyłanie przypomnienia o codziennym aforyzmie na maila (ewentualnie: dostosowanie czcionki w aplikacji)
 """
 
-from tkinter import *
-from time import localtime, strftime
-from playsound import playsound
 from random import choice
+from time import localtime, strftime
+from tkinter import *
+from playsound import playsound
 
+import email
+import ssl
+import smtplib
 
 x = localtime()
 date_today = strftime("%d.%m.%Y", x)
+
+
+def sending_email(email_receiver, aphorism_today):
+    email_sender = 'Aforyzmynakazdydzien@gmail.com'
+    email_password = 'hgfrxleebcbonmtz'
+
+    subject = 'Aforyzmy na każdy dzień'
+    body = f"Aforyzm z dnia {date_today} to : {aphorism_today}"
+
+    em = email.message.EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_password
+    em['Subject'] = subject
+    em.set_content(body)
+
+    contex = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contex) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 
 def sound(func):
@@ -31,12 +54,12 @@ def sound(func):
 
 
 @sound
-def graphic_design(n, list_last_aphorisms):
+def graphic_design(a, list_last_aphorisms):
     window = Tk()
     window.minsize(width=1000, height=250)
     window.geometry("250x250")
 
-    Label(window, text=date_today + " - " + n, font=("Times", 16), wraplength=1000).pack(fill=BOTH, expand=True)
+    Label(window, text=date_today + " - " + a, font=("Times", 16), wraplength=1000).pack(fill=BOTH, expand=True)
     Label(window, text="".join(list_last_aphorisms), justify=LEFT, font=("Times", 9), wraplength=1000).pack(fill=BOTH,
                                                                                                             expand=True)
     window.mainloop()
@@ -50,6 +73,15 @@ def graphic_design(n, list_last_aphorisms):
     # window.mainloop()
 
     # suwak i oprawa graficzna
+
+    with open('email.txt', 'r+') as e:
+        email_receiver = e.read()
+        if len(email_receiver) != 0:
+            sending_email(email_receiver, a)
+        else:
+            email_receiver = input("Podaj email : ")  # uzależnić od interfejsu graficznego
+            e.write(email_receiver)
+            sending_email(email_receiver, a)
 
 
 def main():
